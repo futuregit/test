17e0d19fea1661cde0f62f2b53d8e9c572fabb85
futuregit <tomcis102@hotmail.com>
2021-06-16 04:52:21 -0400

    Added code to scipt.js.

---
+++ script.js
@@ -0,0 +1,16 @@
+const googleDatabase = [
+    'cars.com',
+    'souprecipes.com',
+    'flowers.com',
+    'roads.com',
+    'musclecars.com'
+];
+
+const googleSearch = (searchInput) => {
+    const matches = googleDatabase.filter(website => {
+        return website.includes(searchInput);
+    });
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+};
+
+console.log(googleSearch('soup'));
