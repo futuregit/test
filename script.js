9f7546128f93e41e026cfa9abad2c66fade08acd
futuregit <gmail.com>
2021-06-19 01:09:08 -0400

    Added describe to group tests together.

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ const googleDatabase = [
     'vintagecars.com'
 ];
 
-const googleSearch = (searchInput) => {
-    const matches = googleDatabase.filter(website => {
+const googleSearch = (searchInput, db) => {
+    const matches = db.filter(website => {
         return website.includes(searchInput);
     });
     return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(googleSearch('cars'));
+console.log(googleSearch('cars', googleDatabase));
+
+module.exports = googleSearch;
--- script.test.js
+++ script.test.js
@@ -1,3 +1,29 @@
-it('this is a test', () => {
+const googleSearch = require('./script');
+
+dbMock = [
+    'chess.com',
+    'hamburger.com',
+    'ford.com',
+    'candy.com',
+    'chessgame.com'
+];
+
+describe('googleSearch', () => {
+    it('it is a silly test', () => {
+        expect('hello').toBe('hello');
+    }); 
     
-}) 
+    it('is searching google', () => {
+        expect(googleSearch('testtest', dbMock)).toEqual([]);
+        expect(googleSearch('che', dbMock)).toEqual(['chess.com', 'chessgame.com'])
+    });
+    
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    });
+    
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    });
+});
