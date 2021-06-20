0f8c89e8d1c1ef03127ed4c0fc3dc398bf976489
futuregit <tomcis102@hotmail.com>
2021-06-19 21:19:10 -0400

    Added fetching of api's in file.

--- script2.js
+++ script2.js
@@ -1,1 +1,34 @@
-fetch()
+const fetch = require('node-fetch');
+
+const getPeoplePromise = fetch => {
+    return fetch('https://swapi.dev/api/people')
+        .then(response => response.json())
+        .then(data => {
+            return {
+                count: data.count,
+                results: data.results
+            }
+        })
+};
+
+const getPeople = async(fetch) => {
+    const getRequest = await fetch('https://swapi.dev/api/people');
+    const data = await getRequest.json();    
+    return {
+        count: data.count,
+        results: data.results
+    };
+};
+
+// getPeople(fetch);
+
+const getChuck = async(fetch) => {
+    const getRequest = await fetch('https://api.chucknorris.io/jokes/random');
+    const data = await getRequest.json();    
+    return {
+        url: data.url,
+        value: data.value
+    };
+};
+
+getChuck(fetch)
